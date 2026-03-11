253ae35bc5afed5e108e4b24ac5aa325b97e0188
Harshitha <pinkyharshitha2004@gmail.com>
2026-03-11 11:56:29 +0530

    Interviewpracticequestions

---
+++ Interviewquestions.py/ex.py
@@ -0,0 +1,61 @@
+# * instead of vowels
+# name="iam Harshitha"
+# vowels="aeiouAEIOU"
+# i=0
+# while i<len(name):
+#     if name[i] in vowels:
+#         print("*",end='')
+#     else:
+#         print(name[i],end='') 
+#     i+=1       
+# digits from a number
+# num=1234
+# while num>0:
+#     res=num%10
+#     num=num//10   
+#     print(res,)    
+# palindrome for number
+# num=121
+# num1=num
+# r=0
+# while num>0 :
+#     res=num%10
+#     r=r*10+res
+#     num=num//10 
+# if r==num1:
+#     print(num1,"is a palindrome")
+# else:
+#     print(num,"is not a palindrome")        
+# factors
+# num=10
+# i=1
+# while num>1:
+#     if num%i==0:
+#        print(i)
+#     i+=1
+# prime numbers 
+# i=1
+# num=12
+# count=0
+# while i<=num:
+#     if num%i==0:
+#        count+=1
+#     i+=1
+# if count==2:
+#     print(num,"is prime numbers")    
+# else:
+#     print(num,"is not a prime number")    
+# prime numbers from0-100
+# i=1
+# while i<101:
+#     count=0
+#     j=1
+#     while j<=i:
+#         if i%j==0:
+#            count+=1
+#         j+=1
+#     if count==2:
+#         print(i,"is prime")
+#     i+=1        
+# palindrome for string
+str="madam"
--- LoopingConditions/while.py
+++ LoopingConditions/while.py
@@ -15,11 +15,27 @@
 # while i>0:
 #     print("* "*i)
 #     i-=1
+# i=1
+# while i<6:
+#     print(" "*(5-i)+"* "*i)
+#     i+=1
+# i=4
+# while i>0:
+#     print(" "*(5-i)+"* "*i)
+#     i-=1
+# i=1
+# while i<6:
+#     i+=1
+#     print(i)
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+#     print( )    
 i=1
 while i<6:
-    print(" "*(5-i)+"* "*i)
-    i+=1
-i=4
-while i>0:
-    print(" "*(5-i)+"* "*i)
-    i-=1
+    j=1
+    while j<=i:
+        print(i,end=" ")
+        j+=1   
+    print( )
+    i+=1     
